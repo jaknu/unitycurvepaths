d495749ebeb26e4b7a19b1b95d0f81c1e964a90d
Jakob Svane Knudsen <jakobknudsen@gmail.com>
2014-07-30 22:03:15 +0200

    Code formatting.

--- Assets/FollowBezierCurve/FollowBezierCurve.cs
+++ Assets/FollowBezierCurve/FollowBezierCurve.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class FollowBezierCurve : MonoBehaviour 
 {
-    static Color GIZMO_COLOR_HELPERS = Color.grey;
-    static Color GIZMO_COLOR_CURVE = Color.white;
-    const float GIZMO_SPHERE_SIZE = 0.1f;
-    const int GIZMO_CURVE_LINE_SEGMENTS = 100;
+    static Color GIZMO_COLOR_HELPERS       = Color.grey;
+    static Color GIZMO_COLOR_CURVE         = Color.white;
+    const float  GIZMO_SPHERE_SIZE         = 0.1f;
+    const int    GIZMO_CURVE_LINE_SEGMENTS = 100;
 
     public Vector3[] points;
     public float duration;
@@ -26,17 +26,22 @@ public class FollowBezierCurve : MonoBehaviour
 
     public void Update()
     {
-        _transform.position = bezier(Mathf.Clamp01((Time.time - _startTime) / duration), points);
+        _transform.position = bezier(
+            Mathf.Clamp01((Time.time - _startTime) / duration), 
+            points
+        );
     }
 
     public void OnDrawGizmos()
     {
         Gizmos.color = GIZMO_COLOR_HELPERS;
-        foreach (Vector3 point in points)
+        foreach (Vector3 point in points) {
             Gizmos.DrawSphere(point, GIZMO_SPHERE_SIZE);
+        }
 
-        if(points.Length < 1)
+        if (points.Length < 1) {
             return;
+        }
 
         Vector3 p = points[0];
         for (int i = 1; i < points.Length; i++) {
@@ -47,7 +52,10 @@ public class FollowBezierCurve : MonoBehaviour
         Gizmos.color = GIZMO_COLOR_CURVE;
         p = points[0];
         for (float i = 0f; i <= (float)GIZMO_CURVE_LINE_SEGMENTS; i += 1f) {
-            Vector3 pNew = bezier(Mathf.Clamp01(i / (float)GIZMO_CURVE_LINE_SEGMENTS), points);
+            Vector3 pNew = bezier(
+                Mathf.Clamp01(i / (float)GIZMO_CURVE_LINE_SEGMENTS), 
+                points
+            );
             Gizmos.DrawLine(p, pNew);
             p = pNew;
         }
@@ -55,8 +63,9 @@ public class FollowBezierCurve : MonoBehaviour
 
     static Vector3 bezier(float t, params Vector3[] points)
     {
-        if (points.Length < 1)
+        if (points.Length < 1) {
             throw new System.ArgumentException("A bezier curve cannot be defined with no points.");
+        }
 
         uint n = (uint)points.Length - 1;
         Vector3 sum = Vector3.zero;
@@ -79,12 +88,14 @@ public class FollowBezierCurve : MonoBehaviour
 
     static uint factorial(uint n)
     {
-        if (n <= 1)
+        if (n <= 1) {
             return 1;
+        }
 
         uint result = n;
-        for (uint i = n; i > 1; i--)
+        for (uint i = n; i > 1; i--) {
             result *= i;
+        }
 
         return result;
     }
